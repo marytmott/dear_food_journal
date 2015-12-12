10ca13ed04732d8ca5905cfc8f942fe4059cc805
Mary Mott <mott.maryt@gmail.com>
2015-12-12 00:07:53 -0800

    moved navbar to directive

---
+++ client/js/angular/app/nav/nav.controller.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dearFoodJ.nav')
+    .controller('NavController', NavController);
+
+  NavController.$inject = ['$location', '$rootScope', 'UserService'];
+
+  function NavController($location, $rootScope, UserService) {
+    var vm = this;
+
+// console.log(currentUser);
+    vm.logout = logout;
+    // vm.currentUser = currentUser;
+    vm.currentUser = getCurrentUser();
+    // console.log('user?', vm.currentUser);
+    $rootScope.$on('logout', getCurrentUser);
+    $rootScope.$on('login', getCurrentUser);
+
+    function logout() {
+      UserService.logout();
+      // getCurrentUser();
+      $location.path('/login');
+    }
+
+    function getCurrentUser() {
+      return vm.currentUser = UserService.getCurrentUser();
+      // .then(function(data) {
+      //   console.log(data);
+      //   // vm.currentUser = data;
+      // });
+    }
+  }
+})();
---
+++ client/js/angular/app/nav/nav.directive.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dearFoodJ.nav')
+    .directive('dfNavMain', dfNavMain);
+
+  function dfNavMain() {
+    return {
+      templateUrl: '../partials/nav/nav-main.html',
+      controller: 'NavController',
+      controllerAs: 'vm',
+      restrict: 'E'
+    };
+  }
+})();
