edd55135ff798886b8b8a236d94ee416cf7d8b73
Mary Mott <mott.maryt@gmail.com>
2015-12-12 22:59:09 -0800

    refining API search results on backend and display on frontend

--- server/routes/apiFoods.js
+++ server/routes/apiFoods.js
@@ -8,7 +8,9 @@ router.post('/search', function(req, res) {
   var appId = process.env.NIX_API_ID;
   var apiKey = process.env.NIX_API_KEY;
   var userSearch = req.body.search;
-  var apiSearchLink = 'https://api.nutritionix.com/v1_1/search/' + userSearch + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=' + appId + '&appKey=' + apiKey;
+  var apiSearchLink = 'https://api.nutritionix.com/v1_1/search/'
+    + userSearch + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_serving_size_qty,nf_serving_size_unit,nf_total_fat,nf_total_carbohydrate,nf_dietary_fiber&appId='
+    + appId + '&appKey=' + apiKey;
 
 console.log(apiSearchLink);
   // console.log(req.body.search);
