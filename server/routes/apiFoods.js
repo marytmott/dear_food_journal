31b722499fa7d8bdfef0adea196d83ff4a3338ea
Mary Mott <mott.maryt@gmail.com>
2015-12-13 20:40:31 -0800

    updated api search return fields

--- server/routes/apiFoods.js
+++ server/routes/apiFoods.js
@@ -9,7 +9,7 @@ router.post('/search', function(req, res) {
   var apiKey = process.env.NIX_API_KEY;
   var userSearch = req.body.search;
   var apiSearchLink = 'https://api.nutritionix.com/v1_1/search/'
-    + userSearch + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories,nf_serving_size_qty,nf_serving_size_unit,nf_total_fat,nf_total_carbohydrate,nf_dietary_fiber&appId='
+    + userSearch + '?results=0:20&fields=item_id,item_name,brand_name,nf_serving_size_qty,nf_serving_size_unit,nf_serving_weight_grams,nf_calories,nf_total_fat,nf_total_carbohydrate,nf_dietary_fiber,nf_sugars,nf_protein&appId='
     + appId + '&appKey=' + apiKey;
 
 console.log(apiSearchLink);
