490b566866b170128ac2a0acf2cfbf422601f9c8
Mary Mott <mott.maryt@gmail.com>
2015-12-14 11:35:36 -0800

    updated test response to have appropriate values

--- server/routes/apiFoods.js
+++ server/routes/apiFoods.js
@@ -24,298 +24,391 @@ router.post('/search', function(req, res) {
 //   });
 
 res.send({
-total_hits: 21087,
-max_score: 2.29557,
+total_hits: 411,
+max_score: 3.168178,
 hits: [
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "521b95cb4a56d006d578ba25",
-_score: 2.29557,
+_id: "529e801fea63d4933500cdce",
+_score: 3.168178,
 fields: {
-item_id: "521b95cb4a56d006d578ba25",
-item_name: "Egg Salad Sandwich",
-brand_id: "513fbc1283aa2dc80c00001f",
-brand_name: "Starbucks",
+item_id: "529e801fea63d4933500cdce",
+item_name: "Starbucks Vanilla Frappucino",
+brand_name: "Gandolfo's New York Delicatessen",
+nf_calories: 200,
+nf_total_fat: 3,
+nf_total_carbohydrate: 37,
+nf_dietary_fiber: 0,
+nf_sugars: 31,
+nf_protein: 6,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "537f9baded22ed907af7b505",
-_score: 2.176477,
+_id: "5574f9f2b6b80339177c3c1a",
+_score: 1.955959,
 fields: {
-item_id: "537f9baded22ed907af7b505",
-item_name: "Egg Salad, Deli Sandwich",
-brand_id: "513fbc1283aa2dc80c00001f",
-brand_name: "Starbucks",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "23cb3134e7e8abfb86ab2226",
-_score: 2.118197,
-fields: {
-item_id: "23cb3134e7e8abfb86ab2226",
-item_name: "Egg & Cheddar Breakfast Sandwich",
-brand_id: "513fbc1283aa2dc80c00001f",
-brand_name: "Starbucks",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "54ef3b5c833038202cc8f95b",
-_score: 1.8309906,
-fields: {
-item_id: "54ef3b5c833038202cc8f95b",
-item_name: "Deli Sandwich, Egg Salad",
-brand_id: "51db37c0176fe9790a89903b",
+item_id: "5574f9f2b6b80339177c3c1a",
+item_name: "Frappuccino",
 brand_name: "Starbucks Coffee",
+nf_calories: 300,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 54,
+nf_dietary_fiber: 1,
+nf_sugars: 47,
+nf_protein: 10,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "555ab4be5ee4998464eacdba",
-_score: 1.7856635,
+_id: "5564d0566996de8b6d3d052f",
+_score: 1.7678111,
 fields: {
-item_id: "555ab4be5ee4998464eacdba",
-item_name: "Egg Salad Deli Sandwich",
-brand_id: "51db37c0176fe9790a899039",
+item_id: "5564d0566996de8b6d3d052f",
+item_name: "Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 300,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 54,
+nf_dietary_fiber: 1,
+nf_sugars: 47,
+nf_protein: 10,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "5537a73935bafb0e7d7bc04b",
+_score: 1.7678111,
+fields: {
+item_id: "5537a73935bafb0e7d7bc04b",
+item_name: "Frappuccino",
 brand_name: "Starbucks Grocery",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_calories: 290,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 53,
+nf_dietary_fiber: 0,
+nf_sugars: 46,
+nf_protein: 9,
+nf_serving_size_qty: 13.7,
+nf_serving_size_unit: "fl oz"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "53444cfc7ba4ca15456f561c",
-_score: 0.7603966,
+_id: "51c35e1f97c3e69de4b013f4",
+_score: 1.7678111,
 fields: {
-item_id: "53444cfc7ba4ca15456f561c",
-item_name: "Egg",
-brand_id: "521b95434a56d006cae297f9",
-brand_name: "Bd's Mongolian Grill",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "554f0d1ef2762557602ec862",
-_score: 0.75136155,
-fields: {
-item_id: "554f0d1ef2762557602ec862",
-item_name: "Sandwich",
-brand_id: "554f0d62b31cb526580c21e8",
-brand_name: "Seattle Blues",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "ae497599734c9ec17fce4851",
-_score: 0.74758244,
-fields: {
-item_id: "ae497599734c9ec17fce4851",
-item_name: "Egg",
-brand_id: "513fbc1283aa2dc80c00081c",
-brand_name: "First Watch",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "54f6cfe618344ba86864de39",
-_score: 0.7397537,
-fields: {
-item_id: "54f6cfe618344ba86864de39",
-item_name: "Sandwich",
-brand_id: "54f6cff72011b8ab51ad7b6a",
-brand_name: "Shindig Sammy",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "ae49759903f665984d0bda39",
-_score: 0.73303807,
-fields: {
-item_id: "ae49759903f665984d0bda39",
-item_name: "Egg Sandwich",
-brand_id: "53022ccebf41bf4b1c000001",
-brand_name: "Seasonal Specialties",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "de368420089f365841885f41",
-_score: 0.71659017,
-fields: {
-item_id: "de368420089f365841885f41",
-item_name: "Egg Sandwich",
-brand_id: "de368349c9e7adbb589183d5",
-brand_name: "Well Informed",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "53444b097ba4ca15456f526d",
-_score: 0.71659017,
-fields: {
-item_id: "53444b097ba4ca15456f526d",
-item_name: "Egg Sandwich",
-brand_id: "51db37cf176fe9790a899d49",
-brand_name: "Lyfe Kitchen",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
-}
-},
-{
-_index: "f762ef22-e660-434f-9071-a10ea6691c27",
-_type: "item",
-_id: "54f5620ecd6e20e01186feb8",
-_score: 0.7046365,
-fields: {
-item_id: "54f5620ecd6e20e01186feb8",
-item_name: "Breakfast Sandwich",
-brand_id: "51db37c0176fe9790a89903b",
+item_id: "51c35e1f97c3e69de4b013f4",
+item_name: "Frappuccino",
 brand_name: "Starbucks Coffee",
+nf_calories: 200,
+nf_total_fat: 3,
+nf_total_carbohydrate: 37,
+nf_dietary_fiber: 0,
+nf_sugars: 31,
+nf_protein: 6,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "521b95cb4a56d006d578ba28",
-_score: 0.70304257,
+_id: "5540c5b02afd54fa069811ab",
+_score: 1.6280974,
 fields: {
-item_id: "521b95cb4a56d006d578ba28",
-item_name: "Turkey & Havarti Sandwich",
-brand_id: "513fbc1283aa2dc80c00001f",
-brand_name: "Starbucks",
-nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+item_id: "5540c5b02afd54fa069811ab",
+item_name: "Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 290,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 53,
+nf_dietary_fiber: 0,
+nf_sugars: 46,
+nf_protein: 9,
+nf_serving_size_qty: 405,
+nf_serving_size_unit: "ml"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "5501784494129e2360f2ca38",
-_score: 0.68288755,
+_id: "55943004ef3b6e0b131d5613",
+_score: 1.6280974,
 fields: {
-item_id: "5501784494129e2360f2ca38",
-item_name: "Sandwich, Egg Salad",
-brand_id: "51db37b0176fe9790a8983e0",
-brand_name: "Meijer",
+item_id: "55943004ef3b6e0b131d5613",
+item_name: "Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 200,
+nf_total_fat: 3,
+nf_total_carbohydrate: 37,
+nf_dietary_fiber: 0,
+nf_sugars: 31,
+nf_protein: 6,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "552da1e82becf27830c33f3e",
-_score: 0.6825663,
+_id: "548f0995ee651e3915a655aa",
+_score: 1.6280974,
 fields: {
-item_id: "552da1e82becf27830c33f3e",
-item_name: "Egg",
-brand_id: "51db37be176fe9790a898ec5",
-brand_name: "Snickers",
+item_id: "548f0995ee651e3915a655aa",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 210,
+nf_total_fat: 3.5,
+nf_total_carbohydrate: 38,
+nf_dietary_fiber: 0,
+nf_sugars: 33,
+nf_protein: 7,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "55c99a4f669f4af826fef087",
-_score: 0.6825663,
+_id: "54f6ebb14e9eb9c0518e94d4",
+_score: 1.6280974,
 fields: {
-item_id: "55c99a4f669f4af826fef087",
-item_name: "Egg",
-brand_id: "51db37b1176fe9790a8984ab",
-brand_name: "Polar",
+item_id: "54f6ebb14e9eb9c0518e94d4",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 300,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 54,
+nf_dietary_fiber: 1,
+nf_sugars: 47,
+nf_protein: 10,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "521b95cb4a56d006d578b9ec",
-_score: 0.67531824,
+_id: "55918c9bdb5a8b48603dbc4e",
+_score: 1.6280974,
 fields: {
-item_id: "521b95cb4a56d006d578b9ec",
-item_name: "Starbucks Caramel Iced Coffee",
-brand_id: "513fbc1283aa2dc80c00001f",
-brand_name: "Starbucks",
+item_id: "55918c9bdb5a8b48603dbc4e",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 260,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 47,
+nf_dietary_fiber: 1,
+nf_sugars: 45,
+nf_protein: 9,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "513fc9cb673c4fbc26005c2b",
-_score: 0.67377996,
+_id: "5464c6c49519b24248ec6f98",
+_score: 1.5923419,
 fields: {
-item_id: "513fc9cb673c4fbc26005c2b",
-item_name: "Egg Salad Sandwich",
-brand_id: "513fbc1283aa2dc80c0012ea",
-brand_name: "Izzy's",
+item_id: "5464c6c49519b24248ec6f98",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 200,
+nf_total_fat: 3.5,
+nf_total_carbohydrate: 37,
+nf_dietary_fiber: 0,
+nf_sugars: 32,
+nf_protein: 6,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
 }
 },
 {
 _index: "f762ef22-e660-434f-9071-a10ea6691c27",
 _type: "item",
-_id: "55523d0a06740aca4d077ed0",
-_score: 0.67377996,
+_id: "54f6ebedca74508612a62d35",
+_score: 1.5923419,
 fields: {
-item_id: "55523d0a06740aca4d077ed0",
-item_name: "Egg Salad Sandwich",
-brand_id: "526831133203b9c3390001c8",
-brand_name: "Unknown",
+item_id: "54f6ebedca74508612a62d35",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 290,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 53,
+nf_dietary_fiber: 1,
+nf_sugars: 46,
+nf_protein: 9,
 nf_serving_size_qty: 1,
-nf_serving_size_unit: "serving"
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "55a044ce6b5a279936e8a544",
+_score: 1.5923419,
+fields: {
+item_id: "55a044ce6b5a279936e8a544",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 100,
+nf_total_fat: 3,
+nf_total_carbohydrate: 12,
+nf_dietary_fiber: 0,
+nf_sugars: 11,
+nf_protein: 6,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "55ff6f69c7829ffb34cb32b7",
+_score: 1.5923419,
+fields: {
+item_id: "55ff6f69c7829ffb34cb32b7",
+item_name: "Frappuccino",
+brand_name: "Starbucks Grocery",
+nf_calories: 210,
+nf_total_fat: 3,
+nf_total_carbohydrate: 37,
+nf_dietary_fiber: 0,
+nf_sugars: 32,
+nf_protein: 7,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "55c8d30e5678243326846b2c",
+_score: 1.5923419,
+fields: {
+item_id: "55c8d30e5678243326846b2c",
+item_name: "Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 290,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 53,
+nf_dietary_fiber: 1,
+nf_sugars: 46,
+nf_protein: 9,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "560a33a132ead2742f265094",
+_score: 1.5923419,
+fields: {
+item_id: "560a33a132ead2742f265094",
+item_name: "Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 210,
+nf_total_fat: 3.5,
+nf_total_carbohydrate: 38,
+nf_dietary_fiber: 0,
+nf_sugars: 33,
+nf_protein: 7,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "5518fa93952b050265229cee",
+_score: 0.45888764,
+fields: {
+item_id: "5518fa93952b050265229cee",
+item_name: "Frappuccino, Chilled Coffee Drink",
+brand_name: "Starbucks Coffee",
+nf_calories: 180,
+nf_total_fat: 3,
+nf_total_carbohydrate: 33,
+nf_dietary_fiber: 0,
+nf_sugars: 31,
+nf_protein: 6,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "55599c743f52904c2a37b383",
+_score: 0.45888764,
+fields: {
+item_id: "55599c743f52904c2a37b383",
+item_name: "Chilled Coffee Drink, Frappuccino",
+brand_name: "Starbucks Coffee",
+nf_calories: 290,
+nf_total_fat: 4.5,
+nf_total_carbohydrate: 53,
+nf_dietary_fiber: 0,
+nf_sugars: 46,
+nf_protein: 9,
+nf_serving_size_qty: 1,
+nf_serving_size_unit: "bottle"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "555c92b8cd6394b37a472808",
+_score: 0.45888764,
+fields: {
+item_id: "555c92b8cd6394b37a472808",
+item_name: "Frappuccino, Mocha Light",
+brand_name: "Starbucks Coffee",
+nf_calories: 66,
+nf_total_fat: 2,
+nf_total_carbohydrate: 8,
+nf_dietary_fiber: 0,
+nf_sugars: 8,
+nf_protein: 4,
+nf_serving_size_qty: 200,
+nf_serving_size_unit: "ml"
+}
+},
+{
+_index: "f762ef22-e660-434f-9071-a10ea6691c27",
+_type: "item",
+_id: "545b8d748432234444624e3e",
+_score: 0.45888764,
+fields: {
+item_id: "545b8d748432234444624e3e",
+item_name: "Frappuccino Coffee Drink",
+brand_name: "Starbucks Grocery",
+nf_calories: 180,
+nf_total_fat: 3,
+nf_total_carbohydrate: 33,
+nf_dietary_fiber: 0,
+nf_sugars: 31,
+nf_protein: 7,
+nf_serving_size_qty: 281,
+nf_serving_size_unit: "ml"
 }
 }
 ]
 });
-
-});
-
-
-// find a food
-router.post('/:food_id', function(req, res) {
-
 });
 
 module.exports = router;
