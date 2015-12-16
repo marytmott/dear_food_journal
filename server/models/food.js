7e32d21e0c0ecb0aa1f04d5c1489471a677b9748
Mary Mott <mott.maryt@gmail.com>
2015-12-16 00:08:23 -0800

    updated food model

--- server/models/food.js
+++ server/models/food.js
@@ -17,11 +17,11 @@ var foodSchema = mongoose.Schema({
   fiber: Number,
   protein: Number,
   sugars: Number,
+  nix_id: String,
   user: [{  // to add to user's own personal food db future feature?
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  }],
-  nix_id: String
+  }]
 });
 
 var Food = mongoose.model('Food', foodSchema);
