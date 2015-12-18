1ae2f1b64c4bfb511b09e1ee00fc362df191bfdb
Mary Mott <mott.maryt@gmail.com>
2015-12-17 16:03:41 -0800

    properly changing routes on new day show page

--- client/js/angular/app/days/days.controller.js
+++ client/js/angular/app/days/days.controller.js
@@ -5,14 +5,24 @@
     .module('dearFoodJ.days')
     .controller('DaysController', DaysController);
 
-  DaysController.$inject = ['$routeParams', 'entries'];
+  DaysController.$inject = ['$routeParams', '$location', 'entries'];
 
-  function DaysController($routeParams, entries) {
+  function DaysController($routeParams, $location, entries) {
     var vm = this;
     // only returning meals for now
     vm.date = new Date($routeParams.date);
     // console.log($routeParams);
     vm.meals = entries;
-    vm.sort = '+time'
+    vm.sort = '+time';
+    vm.showNewDate = showNewDate;
+    vm.pickNewDate = null;
+
+    function showNewDate() {
+      // NEED TO MAKE SURE NEW DATE IS PICKED!
+      var newDate = vm.pickNewDate.toLocaleDateString();
+      newDate = newDate.replace(/\//g, '-');
+      $location.path('/journals/' + $routeParams.journal_id + '/days/' + newDate);
+      // $location.path('/journals')
+    }
   }
 })();
