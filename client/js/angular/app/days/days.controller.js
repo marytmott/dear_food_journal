251c9d646bb9b27d8f7a4d36c09b56803b47a4f5
Mary Mott <mott.maryt@gmail.com>
2015-12-17 16:18:27 -0800

    day page calc's todays total nutrition

--- client/js/angular/app/days/days.controller.js
+++ client/js/angular/app/days/days.controller.js
@@ -17,6 +17,33 @@
     vm.showNewDate = showNewDate;
     vm.pickNewDate = null;
 
+
+    function todaysTotalNutrition() {
+      var todaysNutrition = {
+        calories: 0,
+        carbs: 0,
+        fat: 0,
+        fiber: 0,
+        protein: 0,
+        sugars: 0
+      };
+      var currentMeal;
+
+      // move this to separate service as function?
+      for (var i = 0; i < vm.meals.length; i++) {
+        currentMeal = vm.meals[i];
+
+        todaysNutrition.calories += currentMeal.totalNutrition.calories;
+        todaysNutrition.fat += currentMeal.totalNutrition.fat;
+        todaysNutrition.carbs += currentMeal.totalNutrition.carbs;
+        todaysNutrition.fiber += currentMeal.totalNutrition.fiber;
+        todaysNutrition.protein += currentMeal.totalNutrition.protein;
+        todaysNutrition.sugars += currentMeal.totalNutrition.sugars;
+      }
+
+      return vm.todaysNutrition = todaysNutrition;
+    }
+
     function showNewDate() {
       // NEED TO MAKE SURE NEW DATE IS PICKED!
       var newDate = vm.pickNewDate.toLocaleDateString();
@@ -24,5 +51,7 @@
       $location.path('/journals/' + $routeParams.journal_id + '/days/' + newDate);
       // $location.path('/journals')
     }
+
+    todaysTotalNutrition();
   }
 })();
