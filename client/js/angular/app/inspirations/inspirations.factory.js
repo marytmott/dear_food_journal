a73e274dee5e77eb38a05b3f213116a512daf92c
Mary Mott <mott.maryt@gmail.com>
2015-12-19 22:47:07 -0800

    added inspirations factory

--- client/js/angular/app/inspirations/inspirations.factory.js
+++ client/js/angular/app/inspirations/inspirations.factory.js
@@ -8,6 +8,12 @@
   InspirationService.$inject = ['$resource'];
 
   function InspirationService($resource) {
-
+    return {
+      inspirationResource: $resource('/api/journals/:journal_id/inspirations/:inspiration_id',
+        { journal_id: '@journal_id', inspiration_id: '@inspiration_id' },
+          { update: { method: 'PUT'}
+        }
+      )
+    };
   }
 })();
