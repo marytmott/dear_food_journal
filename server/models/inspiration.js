1d3959ae6d3bdfebe6cc9471cf30ffcaf4e60b2e
Mary Mott <mott.maryt@gmail.com>
2015-12-19 21:13:56 -0800

    added new inspiration model

---
+++ server/models/inspiration.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var db = require('./index');
+var Journal = require('./journal');
+
+// this should probably be a few diff models?
+var inspirationSchema = mongoose.Schema({
+  journal: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Journal'
+  }],
+  type: String,
+  imageLink: String,
+  author: String,
+  comment: String,
+  quote: String,
+  tip: String
+});
+
+// hooks?
+
+var Inspiration = mongoose.model('Inspiration', inspirationSchema);
+
+module.exports = Inspiration;
