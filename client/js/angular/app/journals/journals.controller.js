9d3f38d207fe799d23bfa6ffb04fe02767eed490
Mary Mott <mott.maryt@gmail.com>
2016-01-04 10:21:21 -0800

    wip - generating weight change messages for journal page

--- client/js/angular/app/journals/journals.controller.js
+++ client/js/angular/app/journals/journals.controller.js
@@ -28,10 +28,6 @@
     };
     console.log(vm.journal);
 
-    vm.weightChange = {
-      type: 'lost'
-    };
-
     vm.changeAllActive = false;
     vm.changeMode = changeMode;
     vm.changeAll = changeAll;
@@ -69,23 +65,50 @@
       }
     }
 
-    function getWeightChangeMessage() {
+    function calcWeightChange() {
       var trackingWeight = vm.journal.currentWeight && vm.journal.startWeight;
       var weightChange;
+      var typeOfChange;
+      var loseWeight = ' lost ';
+      var gainWeight = ' gained ';
+      var loseWeightGoal = vm.journal.weightChangeType === 'lose';
+      var gainWeightGoal = vm.journal.weightChangeType === 'gain';
 
       if (trackingWeight) {
-        weightChange = vm.journal.startWeight - vm.journal.currentWeight;
+        weightChange = Math.floor(vm.journal.startWeight - vm.journal.currentWeight);
 
-        // if gained weight && user is gaining weight?
+        // if lose weight goal && user lost weight OR gain weight goal and user gained weight
+        if ((weightChange > 0 && loseWeightGoal) || (weightChange < 0 && gainWeightGoal)) {
+          getWeightChangeMessage(loseWeight, weightChange, 'success');
+        } else if ((weightChange < 0 && loseWeightGoal) || (weightChangeType > 0 && gainWeightGoal)) {
+        // if lose weight goal && user gained weight OR gain weight goal and user is losing weight
+          getWeightChangeMessage(gainWeight, weightChange, 'noSuccess');
+
+          vm.weightChangeMessage = 'You\'ve gained ' + weightChange + ' pounds. Don\'t worry, get focused, you can do it!';
+        } else {
+          vm.weightChangeMessage = 'You haven\'t lost any weight yet. Stay focused, you can do it!';
+        }
+
+
         // if (weightChange )
 
         // if gained weight && user is losing weight
 
-        // if lost weight && user is losing weight
 
-        // if lost weight && user is gaining weight
+        // if achieving goals?
 
         // if no change
+      }
+
+    }
+
+    function getWeightChangeMessage(typeOfChange, weightChange, messageName) {
+      if (messageName === 'success') {
+        vm.weightChangeMessage = 'You\'re doing great! You' + typeOfChange + weightChange + ' pounds so far! Keep it up!';
+      } else if (messageName === 'noSuccess') {
+        vm.weightChangeMessage = 'You\'ve' + typeOfChange + weightChange + ' pounds. Don\'t worry, get focused, you can do it!';
+      } else {
+        vm.weightChangeMessage = 'You haven\'t' + typeOfChange + 'any weight yet. Stay focused, you can do it!';
       }
 
     }
@@ -116,11 +139,6 @@
         });
     }
 
-    // function getChangedWeight() {
-    //   if (vm.weightChange.type === 'lost') {
-    //     return vm.journal.updateWeightChange = -
-    //   }
-    // }
     checkUserNamePluralGrammar();
     getWeightChangeMessage();
   }
